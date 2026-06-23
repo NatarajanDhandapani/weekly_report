d5f568d5014e8b560c2b00e7f5c9bbf3c40b8e01
NatarajanDhandapani <ndp15143@yahoo.com>
2026-06-23 16:49:36 +0530

    updated on 09.06.26

--- src/main/java/weekly_report/franchisee.java
+++ src/main/java/weekly_report/franchisee.java
@@ -1,6 +1,6 @@
 package weekly_report;
 
-//updated on 09.06.26 @ 16.40 am
+//updated on 22.06.26 @ 20.30 pm
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.text.ParseException;
@@ -157,7 +157,7 @@ public class franchisee {
 		rightAlignedbody.setFont(font);
 		String[] head = { "Code", "Customer", "ZM", "CSM",  "<Sep-25",   "Oct-25", "Nov-25", "Dec-25", 
 				"Jan-26","Feb-26","Mar-26","Apr-26","May-26","Jun-26", "Jul-26","Aug-26","Sep-26",   "UAC", "Due", "Not Due", "Total",
-				"Plan-Jun", "Act 1-9" };
+				"Plan-Jun", "Act 1-22" };
 		Map<String, String> duedates = new LinkedHashMap<String, String>();
 		duedates.put("Code", "");
 		duedates.put("Name", "");
@@ -190,7 +190,7 @@ public class franchisee {
 		boolean uac = false;
 		SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yyyy");
 		ZoneId defaultZoneId = ZoneId.systemDefault();
-		String notduefm = "22.05.2026"; // --Old 1-8-15-22
+		String notduefm = "08.06.2026"; // --Old 1-8-15-22
 		String trndt = "30.09.2025"; // first column - upto < mmmYY
 		LocalDate dd = ft.parse(notduefm).toInstant().atZone(ZoneId.systemDefault()).toLocalDate().minusDays(1);
 		Date d3 = Date.from(dd.atStartOfDay(defaultZoneId).toInstant());
